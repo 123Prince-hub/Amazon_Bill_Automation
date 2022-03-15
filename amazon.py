883e8a4394ca4a216974493e22498b37d8eff733
Prince Kirad <70063848+123Prince-hub@users.noreply.github.com>
2022-03-15 10:51:51 +0530

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -1,30 +1,34 @@
 import os
+import requests
 import pytesseract
 import xlwings as xw
 from time import sleep
 from selenium import webdriver
+from getmac import get_mac_address as gma
+from selenium.webdriver.common import alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
+
 ws = xw.Book(r'details.xlsx').sheets("data")
 rows = ws.range("A2").expand().options(numbers=int).value
+
 driver = webdriver.Chrome(ChromeDriverManager().install()) 
 driver.maximize_window()
 
+
 # =====================================  function for login  ===================================
-def login():   
+def login():
     email = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')]//following::input").send_keys(row[0])
     button = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')]//following::span").click()
-    password = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::input").send_keys(row[1])
-    signin = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::span").click()
 
 
-# =========================================  function for captcha read  ==========================================
-def captha(): 
-    password = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::input").send_keys(row[1])
+# ======================================  function for captcha read  =======================================
+def captha():
+    driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::input").send_keys(row[1])
     capta= driver.find_element_by_id('auth-captcha-image').screenshot('captcha.png')
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     a = pytesseract.image_to_string(r'captcha.png')
@@ -34,44 +38,60 @@ def captha():
     driver.find_element_by_xpath('//label[contains(text(),"Type characters")]//following::input').send_keys(a.lstrip())
     sleep(1)
     driver.find_element_by_id('signInSubmit').click()
-    # sleep(1)
-    # password.clear()
+
+
+
+# ================================  function for add_password_error using Javascript  ===========================
+# def add_password_error():
+#     execu = '''
+#     var span = document.createElement("SPAN");   
+#     span.type = 'text/javascript';
+#     span.innerHTML = "Message and Data rates may apply.";                   
+#     document.body.appendChild(span);
+#     '''
+#     driver.execute_script(execu)
+#     sleep(2)
 
 
 # ====================================  function for password error  ===================================
-def passError(): 
-    password = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::input").send_keys(row[1])
-    signin = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::span").click()
-
-
-# ====================================  function for email error  ======================================
-def emailError():  
-    Email = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')]//following::span").send_keys(row[0])
+def passError():
+    try:
+        password = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::input").send_keys(row[1])
+        signin = driver.find_element_by_xpath("//label[contains(text(),'Password')]//following::span").click()
+    except:
+        pass
 
 
 # ======================  function for all login, password error capthca, email error  =============================
-def authentication(): 
+def authentication():
     try:
         sleep(1)
         i = 0
-        capthatest = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')] | //h4[contains(text(),'Enter the characters you see')] | //h4[contains(text(),'There was a problem')] | //h1[contains(text(),'Password assistance')]").text
-        while((capthatest=="Enter the characters you see") or (capthatest=="Email or mobile phone number") or (capthatest=="There was a problem") or (capthatest=="Password assistance") and (i<16)):
-            capthatest = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')] |//h4[contains(text(),'Enter the characters you see')] | //h4[contains(text(),'There was a problem')] | //h1[contains(text(),'Password assistance')]").text
+        try:
+            capthatest = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')] | //label[contains(text(),'Type characters')] | //span[contains(text(),'Message and Data rates may apply.')]").text
+        except:
+            pass
+        
+        # add_password_error() # =================== add_password_error function calling ====================
+        try:
+            while((capthatest=="Enter the characters you see") or (capthatest=="Email or mobile phone number") or (capthatest=="Message and Data rates may apply.") and (i<16)):
 
-            if capthatest == "Enter the characters you see":
-                captha()  
-            elif capthatest == "There was a problem":
-                passError()
-            elif capthatest == "Password assistance":
-                emailError()
-            elif capthatest == "Email or mobile phone number":
-                login()
-            # elif capthatest == "Sign-In":
-            #     passError()
-            else:
-                pass
-            i+=1
-            sleep(1)
+                # add_password_error() # ================ add_password_error function calling ====================
+
+                capthatest = driver.find_element_by_xpath("//label[contains(text(),'Email or mobile phone number')] | //label[contains(text(),'Type characters')] | //span[contains(text(),'Message and Data rates may apply.')]").text
+
+                if capthatest == "Message and Data rates may apply.":
+                    passError()
+                if capthatest == "Type characters":
+                    captha()
+                if capthatest == "Email or mobile phone number":
+                    login()
+                else:
+                    pass
+                i+=1
+                sleep(1)
+        except:
+            pass    
     except:
         del capthatest
 
@@ -81,20 +101,22 @@ def otp():
     try:
         send_otp = driver.find_element_by_xpath('//*[@id="continue"]').click()
         sleep(30)
-        # ok_button = driver.find_element_by_xpath('//*[@id="cvf-submit-otp-button"]/span/input').click()
     except:
         pass
 
 
-# ===================================  function for dissmiss all popups  ================================
-def popups():  
+# ==========================================  function for Login Approve  ======================================
+def login_approve():
     try:
-        pass
+        approve = driver.find_element_by_xpath('//span[text()="For your security, approve the notification sent to:"]')
+        sleep(60)
     except:
-        pass
+        pass       
 
 
-# =====================================  start automation from here  ====================================
+# -------------------------------------  ##########################  ------------------------------------
+# =====================================  Start Automation From Here  ====================================
+# -------------------------------------  ##########################  ------------------------------------
 
 num = 2
 for row in rows:
@@ -106,7 +128,7 @@ for row in rows:
             state_dropdown = driver.find_element_by_xpath('//span[text()="Select State"]').click()
             state = driver.find_element_by_xpath('//a[text()="Rajasthan"]').click()
 
-            board = row[3]
+            board = row[3].strip()
             board_dropdown = WebDriverWait(driver, 30).until(
                 EC.presence_of_element_located((By.XPATH, '//span[text()="Select Electricity Board to proceed"]'))
             )
@@ -116,42 +138,73 @@ for row in rows:
             k_number = WebDriverWait(driver, 30).until(
                 EC.presence_of_element_located((By.XPATH, '//label[contains(text(), "K Number")]//following::input'))
             )
+
+            # driver.get_cookies()
+            # driver.delete_all_cookies()
+
             k_number.send_keys(row[4])
             fetch_bill = driver.find_element_by_xpath('//span[text()="Fetch Bill"]').click()
             
-            element = WebDriverWait(driver, 50).until(
-                EC.presence_of_element_located((By.XPATH, '//span[contains(text(),"Continue to Pay")]'))
-            )
-            
+            sleep(2)
+            bypass_id = driver.execute_script('document.querySelector("#paymentBtnId-announce").setAttribute("type", "submit")')
+
+            try:
+                popup = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="a-popover-1"]/div'))
+                    )
+                popup_text = popup.text 
+                # popup_text = popup_text.replace("\n", "")
+                sleep(1)
+                ws.range("H"+str(num)).value = "NA"
+                ws.range("I"+str(num)).value = popup_text
+                sleep(1)
+
+            except:
+                element = WebDriverWait(driver, 50).until(
+                    EC.presence_of_element_located((By.XPATH, '//span[contains(text(),"Continue to Pay")]'))
+                )
+
             sleep(1)
             res_amount = element.text
             res_amount = res_amount[17:]
             ws.range("G"+str(num)).value = res_amount
 
-            bypass_id = driver.execute_script('document.querySelector("#paymentBtnId-announce").setAttribute("type", "submit")')
-            element = WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Continue to Pay")]'))
-            )
-            element.click()
-
-            authentication() # =========================== login & captcha function calling ============================
+            sleep(1)
+            amount = ws.range("F"+str(num)).value
+            response_amount = ws.range("G"+str(num)).value     
             
-            button = driver.find_element_by_xpath('//span[text()="Place Order and Pay"]')
-            driver.execute_script("arguments[0].click();", button)
-            sleep(10)
-            otp()
+            if amount == response_amount:
+                sleep(2)
+                element = WebDriverWait(driver, 30).until(
+                    EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Continue to Pay")]'))
+                )
+                element.click()
+            else:
+                ws.range("H"+str(num)).value = "NA"
+                ws.range("I"+str(num)).value = "Amount miss Match"
+
+
+            authentication() # ========================= login & captcha function calling ==========================
+            login_approve()  # =====================  Login Approve screen Hold function calling ===================
+            
+
+            pay = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@name="ppw-widgetEvent:SetPaymentPlanSelectContinueEvent"]')) )
+            pay.click()
+            
+            sleep(15)
+            otp()  # =====================  Otp screen function Calling function calling  ========================
             
             sleep(40)
             try:
-                success = WebDriverWait(driver, 30).until(
+                success = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="deep-dtyp-success-alert"]/div/h4'))
                 )
 
-                pending = WebDriverWait(driver, 30).until(
+                pending = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="deep-dtyp-pending-widget"]/div/div/h4'))
                 )
 
-                fail = WebDriverWait(driver, 30).until(
+                fail = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="deep-dtyp-failed-widget"]/div/div/h4'))
                 )
 
@@ -174,6 +227,16 @@ for row in rows:
             except:
                 pass
 
+            sleep(1)
+            try:
+                order = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Order #")]'))
+                )
+                order_id = order.text
+                order_ids = order_id[8:]
+            except:
+                pass
+
             if "successful" in status:
                 ws.range("H"+str(num)).value = bbps_num
                 ws.range("I"+str(num)).value = "Success"
@@ -181,14 +244,17 @@ for row in rows:
             elif "pending" in status:
                 ws.range("H"+str(num)).value = "NA"
                 ws.range("I"+str(num)).value = "Pending"
+                ws.range("J"+str(num)).value = order_ids
 
             else:
                 ws.range("H"+str(num)).value = "NA"
                 ws.range("I"+str(num)).value = "Fail"
         
         except:
-            ws.range("H"+str(num)).value = "NA"
-            ws.range("I"+str(num)).value = "Server not response"
+            stat = ws.range("I"+str(num)).value
+            if stat =="Fail" or stat == "Server not response" or stat==None:
+                ws.range("H"+str(num)).value = "NA"
+                ws.range("I"+str(num)).value = "Server not response"
 
 
     # =============================== For Remove Captcha Image ===================================
@@ -197,7 +263,9 @@ for row in rows:
     except:
         pass
 
+
     num += 1
 driver.close()
 
-exit = input("\n\n\n========================= press any key to exit ============================")
+
+input("\n\n\n========================= Press Enter  to exit ============================")
