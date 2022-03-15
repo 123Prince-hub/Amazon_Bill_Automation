1fb7c7fa3f126b1befcf8ec746a79ec9120f1c44
Prince Kirad <70063848+123Prince-hub@users.noreply.github.com>
2022-03-15 11:18:29 +0530

    update req.py file

--- req.py
+++ req.py
@@ -19,33 +19,3 @@ else:
     print("Your registration key not genrate")
 
 input("\n\n\n========================= Press Enter  to exit ============================")
-
-
-
-
-
-
-
-
-
-
-# import random
-# import base64
-# from Crypto.Cipher import AES
-
-# rand_num = random.randint(100000, 999999)
-# rand_num = str(rand_num)+"aaaaaaaa"
-
-# secret_key = b'1121121121121212'
-
-# msg_text = rand_num+"@"+system_mac
-# msg_text = msg_text.encode().rjust(32)
-
-# cipher = AES.new(secret_key,AES.MODE_ECB) # never use ECB in strong systems obviously
-
-# encoded = base64.b64encode(cipher.encrypt(msg_text))
-# print(encoded)
-
-# decoded = cipher.decrypt(base64.b64decode(encoded))
-# print(decoded)
-
